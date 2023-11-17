be08d14f33a244b5527f639342b55c49204cebbb
fernando-henrique2001 <fernandohenrique.gp@gmail.com>
2023-11-16 22:02:28 -0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,34 +4,32 @@ import { Card } from "@/components/Card";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-
 export default function Home() {
   const [documents, setDocuments] = useState<any>();
   useEffect(() => {
     const getData = async () => {
-      const response = await fetch("http://localhost:3000/documentos")
-      const data = await response.json()
+      const response = await fetch("http://localhost:3000/documentos");
+      const data = await response.json();
       setDocuments(data);
-    }
+    };
 
-    getData()
+    getData();
+  }, []);
 
-  })
+  return (
+    documents && (
+      <div className="w-screen h-screen px-20 py-5 flex flex-col items-center">
+        <h1 className="text-lg">Minhas Publicações</h1>
+        <div className="flex justify-end w-[60rem]">
+          <Button text="Nova Publicação" />
+        </div>
 
-
-
-  return (documents &&
-    <div className="w-screen h-screen px-20 py-5 flex flex-col items-center">
-      <h1 className="text-lg">Minhas Publicações</h1>
-      <div className="flex justify-end w-[60rem]">
-        <Button text="Nova Publicação" />
+        <div className="flex flex-col gap-4 mt-10">
+          {documents.map((doc: any) => (
+            <Card key={doc.title} status={doc.status} title={doc.title}></Card>
+          ))}
+        </div>
       </div>
-
-      <div className="flex flex-col gap-4 mt-10">
-        {documents.map((doc:any) => (
-          <Card key={doc.title} status={doc.status} title={doc.title}></Card>
-        ))}
-      </div>
-    </div>
+    )
   );
 }
