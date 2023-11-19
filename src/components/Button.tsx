a872817b6077ac9f1d1c5de8c9761f6a032d2576
LSAquino <lucas.silva22@live.com>
2023-11-19 15:33:55 -0300

    corrigindo a pataquada

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,16 @@
 "use client";
 import React from "react";
+import Link from 'next/link';
+
 
 export const Button = ({ text }: { text: string }) => {
   return (
     <div className="w-52 h-8 rounded-xl border border-black flex justify-center items-center mt-10 ">
-      <button className="bg-gray hover:bg-gray-dark text-black font-bold py-2 px-4 rounded">
-        {text}
-      </button>
+      <Link href="/publish">
+        <div className="bg-gray hover:bg-gray-dark text-black font-bold py-2 px-4 rounded">
+          {text}
+        </div>
+      </Link>
     </div>
   );
 };
